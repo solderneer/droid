2be9434716810d7549bbc1e70d396e91bcedd33f
Sudharshan <solderneer@gmail.com>
2020-01-12 13:51:34 +0800

    feat: configured memory and cargo run for stm32f4

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-#![no_std]
+// ! Prints "Hello, world!" on the host console using semihosting
+
 #![no_main]
+#![no_std]
 
-// pick a panicking behavior
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate panic_halt;
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    hprintln!("Hello, world!").unwrap();
 
-    loop {
-        // your code goes here
-    }
+    // exit QEMU
+    // NOTE do not run this on hardware; it can corrupt OpenOCD state
+    // debug::exit(debug::EXIT_SUCCESS);
+
+    loop {}
 }
