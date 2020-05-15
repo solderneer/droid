b8bbb294efb730a2565b90322440dc927770b182
Sudharshan <solderneer@gmail.com>
2020-05-15 14:27:53 +0800

    refactor: messing around with cinder tutorial

--- sim/src/main.cpp
+++ sim/src/main.cpp
@@ -1,3 +1,8 @@
+#include <cinder/Vector.h>
+#include <cinder/app/AppBase.h>
+#include <cinder/gl/wrapper.h>
+#include <glm/fwd.hpp>
+#include <ostream>
 #include <stdlib.h>
 
 #include "cinder/app/App.h"
@@ -24,7 +29,43 @@ void Template::update()
 }
 void Template::draw()
 {
+  gl::clear();
+  gl::setMatricesWindow(getWindowSize());
+
+  // Preserve the default model matrix
+  gl::pushModelMatrix();
+  gl::translate(getWindowCenter());
+
+  int numCircles = 16;
+  float radius = getWindowHeight() / 2 - 30;
+
+  for (int i = 0; i < numCircles; ++i) {
+    float rel = i / (float) numCircles;
+    float angle = rel * (M_PI * 2);
+    vec2 offset(cos(angle), sin(angle));
+
+    // preserve the Model matrix
+		gl::pushModelMatrix();
+		// move to the correct position
+		gl::translate( offset * radius );
+		// set the color using HSV color
+		gl::color( Color( CM_HSV, rel, 1, 1 ) );
+		// draw a circle relative to Model matrix
+		gl::drawStrokedCircle( vec2(), 30 );
+		// restore the Model matrix
+		gl::popModelMatrix();
+
+    std::cout << i << std::endl;
+  }
+
+  // draw a white circle at window center
+	gl::color( Color( 1, 1, 1 ) );
+	gl::drawSolidCircle( vec2(), 15 );
+
+	// restore the default Model matrix
+	gl::popModelMatrix();
 }
+
 void Template::cleanup()
 {
 }
