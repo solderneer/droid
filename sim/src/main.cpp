086aee95120eb486c4f70110ed924f3d9b790dce
Sudharshan <solderneer@gmail.com>
2020-05-19 11:48:39 +0800

    feat: adding full linkage system for legs

--- sim/src/main.cpp
+++ sim/src/main.cpp
@@ -1,5 +1,6 @@
 #include <cinder/Easing.h>
 #include <cinder/CinderImGui.h>
+#include <cinder/GeomIo.h>
 #include <cinder/Vector.h>
 #include <cinder/app/AppBase.h>
 #include <cinder/gl/draw.h>
@@ -28,20 +29,25 @@ class Droid : public App {
 	  gl::BatchRef	mServo1;
 	  gl::BatchRef	mServo2;
     gl::BatchRef  mServo3;
+    gl::BatchRef  mLeg;
 
     vec3 jointPosition = vec3(0);
-    vec3 camPos = vec3(0.0f, 0.0f, 15.0f);
+    vec3 camPos = vec3(0.0f, 25.0f, 40.0f);
     vec3 camAngle = vec3(0);
 
     vec3 servo2Pos = vec3(3.4, 0.3, 0);
+    vec3 servo3Pos = vec3(0, 6.0, 0);
+    vec3 legPos = vec3(2.0, 1.5, 0);
 };
 
 void Droid::setup() {
   auto lambert = gl::ShaderDef().lambert().color();
   gl::GlslProgRef shader = gl::getStockShader(lambert);
 
-  mServo1 = gl::Batch::create(geom::Cube().size(4, 3.5, 1.9), shader);
-  mServo2 = gl::Batch::create(geom::Cube().size(1.9, 4, 3.5), shader);
+  mServo1 = gl::Batch::create(geom::Cube().size(4.0, 3.5, 1.9), shader);
+  mServo2 = gl::Batch::create(geom::Cube().size(1.9, 4.0, 3.5), shader);
+  mServo3 = gl::Batch::create(geom::Cube().size(1.9, 4.0, 3.5), shader);
+  mLeg = gl::Batch::create(geom::Cone().height(10.0).radius(1, 0.2).direction(vec3(1, 0, 0)), shader);
 
   mCam.lookAt(camPos, camAngle);
 
@@ -59,6 +65,7 @@ void Droid::update() {
   ImGui::Begin("Joint Settings");
   ImGui::SliderFloat("Joint 1", &jointPosition[0], -0.5 * M_PI, 0.5 * M_PI);
   ImGui::SliderFloat("Joint 2", &jointPosition[1], 0, -M_PI);
+  ImGui::SliderFloat("Joint 3", &jointPosition[2], -0.5 * M_PI, 0.5 * M_PI);
   ImGui::End();
 
   // Camera settings
@@ -67,9 +74,11 @@ void Droid::update() {
   ImGui::SliderFloat3("Angle", &camAngle, 0.00f, 2 * M_PI);
   ImGui::End();
 
-  // Servo2 Adjust
-  ImGui::Begin("Servo Constraint Adjust");
+  // Linkage Constraint Adjust
+  ImGui::Begin("Linkage Constraint Adjust");
   ImGui::SliderFloat3("Servo 1 - Servo 2", &servo2Pos, 0, 5);
+  ImGui::SliderFloat3("Servo 2 - Servo 3", &servo3Pos, 0, 15);
+  ImGui::SliderFloat3("Servo 3 - Leg", &legPos, 0, 15);
   ImGui::End();
 
   ImGui::Render();
@@ -86,10 +95,13 @@ void Droid::draw() {
   gl::setMatrices(mCam);
 
   gl::ScopedModelMatrix scpModelMtx;
+
+  /* Servo 2 transformation and creation */
   gl::rotate( angleAxis( jointPosition[0], vec3( 0, 1, 0 ) ) );
   mServo1->draw();
   gl::drawCoordinateFrame(3, 0.2, 0.05);
 
+  /* Servo 2 transformation and creation */
   gl::translate(servo2Pos);
   gl::rotate( angleAxis( jointPosition[1], vec3( 0, 0, 1 ) ) );
   
@@ -102,6 +114,15 @@ void Droid::draw() {
   gl::translate(rotationPoint);
 
   mServo2->draw();
+
+  /* Servo 3 transformation and creation */
+  gl::translate(servo3Pos);
+  mServo3->draw();
+
+  /* Leg transformation and creation */
+  gl::translate(legPos);
+  gl::rotate( angleAxis( jointPosition[2], vec3( 0, 0, 1 ) ) );
+  mLeg->draw();
 }
 
 // Unused stub
